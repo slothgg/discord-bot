7218c973ec8b275e4bf0f30cfb635bf5b5b89fb1
HyunTaek Oh <oht222@naver.com>
2022-09-24 04:28:14 +0900

    add: DiscordFactory init 시 디스코드 봇 login 단계 추가

--- src/common/core/index.ts
+++ src/common/core/index.ts
@@ -12,6 +12,11 @@ export default class DiscordFactory extends Client {
   }
 
   public async init() {
-    this.logger.log('');
+    this.logger.log('Initializing...');
+    this.logger.log('Initialize Done');
+
+    await this.login(process.env.TOKEN)
+      .then(() => this.logger.log('Connected to Discord Server'))
+      .catch((err) => this.logger.error(err.message));
   }
 }
