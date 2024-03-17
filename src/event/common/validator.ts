9f29c611e2d5155b23adb2fd14bebda82768b1c0
HyunTaek Oh <oht222@naver.com>
2024-03-18 01:38:02 +0900

    fix: 유저이름 추출 방식 변경

--- src/event/common/validator.ts
+++ src/event/common/validator.ts
@@ -1,7 +1,8 @@
 export const validInput = (
   input: string,
 ): { username: string; valid: RegExpMatchArray } => {
-  const username = input.substring(5, input.length).trim();
+  const content = input.split(' ');
+  const username = content[1];
 
   const valid = username.match(
     /[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g,
